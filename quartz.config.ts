c444f9a10fcdd133d56516c274ad46aea130ccd5
Balou97 <rainhardneuhauser@gmail.com>
2024-08-03 18:26:28 +0200

    Quartz sync: Aug 3, 2024, 6:26 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -30,7 +30,7 @@ const config: QuartzConfig = {
         lightMode: {
           light: "#faf8f8",
           lightgray: "#e5e5e5",
-          gray: "#fbe2c5",
+          gray: "#b8b8b8",
           darkgray: "#4e4e4e",
           dark: "#2b2b2b",
           secondary: "#284b63",
@@ -39,7 +39,7 @@ const config: QuartzConfig = {
           textHighlight: "#fff23688",
         },
         darkMode: {
-          light: "#161618",
+          light: "#fbe2c5",
           lightgray: "#393639",
           gray: "#646464",
           darkgray: "#d4d4d4",
